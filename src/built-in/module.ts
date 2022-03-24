271169d7a3933a97569a5cd73df61de3c33453b5
revers3ntropy <joseph.coppin@gmail.com>
2022-03-24 20:24:20 +0000

    start of module redo

--- src/built-in/module.ts
+++ src/built-in/module.ts
@@ -2,22 +2,14 @@ import type {ESError} from '../errors';
 import type {Context} from '../runtime/context';
 import type {NativeObj} from '../runtime/primitives/primitive';
 import type {Primitive} from '../runtime/primitiveTypes';
-import type {BuiltInFunction} from '../util/util';
+import type {BuiltInFunction, dict} from '../util/util';
 
 export type moduleValues = number | string | moduleValues[] | Primitive | BuiltInFunction;
 
-export type JSModule = {[key: string]: moduleValues};
+export type JSModule = dict<moduleValues>;
 
 export type JSModuleParams = {
-    https?: NativeObj,
-    http?: NativeObj,
-    fs?: NativeObj,
-    mysql?: NativeObj,
-    fetch?: NativeObj,
-    context?: Context,
-    print: (...args: string[]) => void,
-    path?: NativeObj,
-
+    context: Context;
     [k: string]: NativeObj;
 };
 
